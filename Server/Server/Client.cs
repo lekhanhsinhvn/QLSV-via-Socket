d6d7ba5ec5a33d56caaab2e08696c9405477aaf0
Sinh <sinhlkd00672@fpt.edu.vn>
2018-09-14 19:21:09 +0700

    Server

---
+++ Server/Server/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace Server
+{
+    class Client
+    {
+        private NetworkStream stream;
+        private StreamReader reader;
+        private StreamWriter writer;
+        public Socket socket
+        {
+            get;
+            set;
+        }
+
+
+        public Client(Socket socket)
+        {
+            this.socket = socket;
+            stream = new NetworkStream(socket);
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream);
+            writer.AutoFlush=true;
+            Thread t3 = new Thread(() => Readhanle());
+            t3.Start();
+            
+        }
+        public void Readhanle()
+        {
+            string str;
+            while ((str=reader.ReadLine())!=null)
+            {
+                string[] data = Regex.Split(str, "#@#");
+                if (data[0].Equals(" "))
+                {
+
+                }
+            }
+        }
+        public void Clientclose()
+        {
+            socket.Close();
+        }
+    }
+}
---
+++ Server/Server/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Server
+    {
+        private IPAddress address = IPAddress.Parse("127.0.0.1");
+        private const int PORT_NUMBER = 2222;
+        public List<Client> client_list
+        {
+            get;
+            set;
+        }
+
+        public void ServerStart()
+        {
+            client_list = new List<Client>();
+            TcpListener listener = new TcpListener(address, PORT_NUMBER);
+            listener.Start();
+            Console.WriteLine("Server started on " + listener.LocalEndpoint);
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+                Client client = new Client(socket);
+                client_list.Add(client);
+            }
+            
+        }
+
+        public void delClient(Socket socket)
+        {
+            foreach(Client cl in client_list)
+            {
+                if (SocketConnected(cl.socket))
+                {
+                    client_list.Remove(cl);
+                }
+            }
+        }
+
+        bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+    }
+}
